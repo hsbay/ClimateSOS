fe0bcbffbc32c4acea842811f990e28b74ea43d1
Shannon A. Fiume <4840719+safiume@users.noreply.github.com>
2026-06-14 18:54:17 -0700

    Add common ClimateSOS headers to v0.7 runtime files

--- climatesos/states.py
+++ climatesos/states.py
@@ -1,3 +1,6 @@
+# Climate State transition model OS
+# Github Project Code: https://github.com/hsbay/ClimateSOS, CC-BY 4.0 2026 @safiume
+
 """Runtime state enums for the ClimateSOS v0.7 toy evaluator."""
 
 from enum import Enum
